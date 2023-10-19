d7050516566a40395a77bc224a7ad4e799d341e6
codeylee05 <mlefa5002@gmail.com>
2023-10-19 13:27:29 +0200

    Create profile form styles

---
+++ src/scripts/create-profile.js
@@ -0,0 +1,10 @@
+profileFormDivs = document.querySelector(".pf-div")
+submitIcons = document.querySelectorAll("svg")
+
+submitIcons.forEach(submitIcon => {
+    submitIcon.onclick = handleSubmit()
+})
+
+function handleSubmit() {
+
+}
