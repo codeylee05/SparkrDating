9b7c98a0b576e5ff6741e157dc56fc4007c2bfa9
codeylee05 <mlefa5002@gmail.com>
2023-10-19 15:24:21 +0200

    Javascript for Form rendering. Form styles

--- src/scripts/create-profile.js
+++ src/scripts/create-profile.js
@@ -1,10 +1,30 @@
-profileFormDivs = document.querySelector(".pf-div")
-submitIcons = document.querySelectorAll("svg")
+const submitIcons = document.querySelectorAll(".svg-cont");
 
 submitIcons.forEach(submitIcon => {
-    submitIcon.onclick = handleSubmit()
+    submitIcon.addEventListener("click", handleSubmit);
 })
 
-function handleSubmit() {
+function handleSubmit(event) {
+    console.log("handleSubmit triggered")
+
+    const target = event.target
+    //console.log(target)
+    const parentTarget = target.parentElement
+    // console.log(parentTarget)
+    const grandparentTarget = parentTarget.parentElement
+    // console.log(grandparentTarget)
+    const greatparentTarget = grandparentTarget.parentElement
+    //console.log(greatparentTarget)
+    const profileFormDiv = greatparentTarget.parentElement
+    //console.log(profileFormDiv)
+
+    profileFormDiv.classList.remove("active")
+    profileFormDiv.classList.add("inactive")
+
+    profileFormDiv.nextElementSibling.classList.remove("inactive")
+    profileFormDiv.nextElementSibling.classList.add("active")
 
 }
+
+
+
